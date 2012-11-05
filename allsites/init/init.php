bb39d2199bd66b306ff6a24afa27f603ab3f418c
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2012-11-05 01:51:57 +0100

    ddd

---
+++ allsites/init/init.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once $v[path][fw] . '/func/templates/templates.php';
+
+$v[debug]=1;
+
+$v[where][view]="categorias";
+$v[where][id]=122;
+
+
+echo loadChild('objt','footer');
+
+
+
+
+	
+
+
+?>
---
+++ allsites/objt/php/allviews/footer.php
@@ -0,0 +1,8 @@
+<?php
+
+
+
+$valores[valor1]="11111";
+$valores[valor2]="22222";
+
+?>
