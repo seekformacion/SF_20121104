3f32d39c15b63b3f1e9ef9df96bb5183bb53db7d
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2012-11-08 16:09:16 +0100

    ff

--- cursodecursos.com/init/php/allviews/config.php
+++ cursodecursos.com/init/php/allviews/config.php
@@ -15,9 +15,9 @@ $v['path']['c_js']="/cskin/js"; #ruta httpd relativa para almacenar los css en p
 
 
 $v['path']['localBasePathimg']=					 "/img";
-$v['path']['cloudBasePathimg']=					 "https://s3-eu-west-1.amazonaws.com/seekf/img";
+$v['path']['cloudBasePathimg']=					 "http://d3af3ipdddujzp.cloudfront.net/img";
 
 $v['path']['baseURLskin'][1]=""; ## baseURL del SKIN local
-$v['path']['baseURLskin'][2]="https://s3-eu-west-1.amazonaws.com/seekf"; ## baseURL del SKIN en CLOUD
+$v['path']['baseURLskin'][2]="http://d3af3ipdddujzp.cloudfront.net"; ## baseURL del SKIN en CLOUD
 
 ?>
