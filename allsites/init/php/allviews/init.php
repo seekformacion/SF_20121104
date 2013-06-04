79820e07cde5db32c5ed096713d08044b189469a
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-06-04 12:54:22 +0200

    ddd

--- allsites/init/php/allviews/init.php
+++ allsites/init/php/allviews/init.php
@@ -13,7 +13,7 @@ $v['where']['id']=122;
 if(strlen(str_replace('/cat=', '', $v['where']['url'])) < strlen($v['where']['url'])) {$v['where']['view']="categorias"; 	$sacos=explode('=', $v['where']['url']); 	$v['where']['id']=$sacos[1];};
 if(strlen(str_replace('/cur=', '', $v['where']['url'])) < strlen($v['where']['url'])) {$v['where']['view']="cursos"; 		$sacos=explode('=', $v['where']['url']); 	$v['where']['id']=$sacos[1];};
 
-echo "Tipo:" . $v['where']['view'] . " id: " . $v['where']['id'] . " <br>\n";
+#echo "Tipo:" . $v['where']['view'] . " id: " . $v['where']['id'] . " <br>\n";
 
 
 
