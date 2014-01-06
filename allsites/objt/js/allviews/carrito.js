344576aee42958e28be2daef2a14ac3a66295ebf
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-01-06 14:24:29 +0100

    dd

--- allsites/objt/js/allviews/carrito.js
+++ allsites/objt/js/allviews/carrito.js
@@ -384,7 +384,7 @@ var pagina="https://www.facebook.com/dialog/feed?app_id=673960869311429&display=
 var opciones="toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=yes, width=508, height=365, top=85, left=140";
 }
 
-window.open(pagina,"",opciones);
+window.open(pagina,"soc",opciones);
 	
 }
 
