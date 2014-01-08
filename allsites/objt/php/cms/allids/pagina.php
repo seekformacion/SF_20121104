a1c06944fea9054b021f9bfd1dd8fe9147dee90e
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-01-08 13:15:10 +0100

    eee

--- allsites/objt/php/cms/allids/pagina.php
+++ allsites/objt/php/cms/allids/pagina.php
@@ -1,44 +1,16 @@
 <?php 
 
-//includeFUNC('categorias');
-//includeFUNC('sacaCursos');
 
 $idcms=$v['where']['id'];
-//catsINF($idcat);
 
 
+$inf=DBselect("select html from skf_cms where t_id=$idcms;");
+if(array_key_exists(1, $inf)){$cms=$inf[1]['html'];}
 
+$Datos['cms']=$cms;
 
-//$Datos['pagTittle']=$v['where']['pagTittle'];
-
-
-
-
-//$Datos['breadcrumbs']=breadCRUMBS($idcat);
-
-//$Datos['sliders']=loadChild('objt','sliders');
-
-//$Datos['sliders']=loadChild('objt','cabhome');
-
-//$Datos['add_CUR_cent']=loadChild('objt','add_CUR_cent');
-
-//$Datos['catsinf']=loadChild('objt','catsinf');
-
-//$Datos['listCursos']=loadChild('objt','listCursos');
-
-
-
-//$Datos['descTXTcat']=loadChild('objt','descTXTcat');
 $Datos['adW_LD']=loadChild('objt','adW_LD');
 
-
-//$Datos['tod_CUR']=loadChild('objt','tod_CUR');
-//$Datos['bloqueGEO']=loadChild('objt','bloqueGEO');
-//$Datos['bloqueONLINE']=loadChild('objt','bloqueONLINE');
-//$Datos['bloqueDISTANCIA']=loadChild('objt','bloqueDISTANCIA');
-
-
-//$Datos['masCATS']=loadChild('objt','masCATS');
 
 
 $Datos['navBAR']=loadChild('objt','navBAR');
