25c9a54384731af31d3a3de4c29da9b548c7c31c
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-01-08 20:02:30 +0100

    fff

--- allsites/objt/php/cms/allids/header.php
+++ allsites/objt/php/cms/allids/header.php
@@ -8,9 +8,11 @@ $Datos['imgMaskCat']=loadIMG("mascaras/mascarafondocat.png");
 $Datos['home']="http://" . $v['where']['site'];
 
 
+
 if($v['where']['idp']==1){$self='<a class="color" alt="Curso de cursos" href="http://cursodecursos.com">Curso cursos</a>';};
 if($v['where']['idp']==2){$self='<a class="color" alt="Master de Masters" href="http://masterenmasters.com">Master en: Masters</a>';};
 if($v['where']['idp']==3){$self='<a class="color" alt="Fp - Técnicos - Ciclos - Formación Profesional" href="http://fp-formacionprofesional.com">Fp - Ciclos - Técnicos: Formación Profesional</a>';};
+if($v['where']['idp']==4){$self='<a class="color" alt="Preparacion oposiciones a" href="http://oposicionesa.com">Oposiciones a</a>';};
 
 
 $Datos['self']=$self;
