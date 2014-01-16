987f2d54888a1bfde7aec37825f86a86a2c9ef1b
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-01-16 11:39:43 +0100

    ddd

--- allsites/objt/js/allviews/init.js
+++ allsites/objt/js/allviews/init.js
@@ -154,8 +154,8 @@ function getCookie(c_name)
 var i,x,y,ARRcookies=document.cookie.split(";");
 for (i=0;i<ARRcookies.length;i++)
 {
-  x=ARRcookies[i].substr(0,$.inArray("=",ARRcookies[i]));
-  y=ARRcookies[i].substr(($.inArray("=",ARRcookies[i]))+1);
+  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
   x=x.replace(/^\s+|\s+$/g,"");
   if (x==c_name)
     {
