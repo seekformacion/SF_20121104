7208f8e697346763d6f9fcc392351eab593351c8
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-14 19:55:48 +0100

    rrr

--- allsites/ajx/fp-test.php
+++ allsites/ajx/fp-test.php
@@ -12,6 +12,33 @@ print_r($_GET);
 <br>
 <?php
 print_r($_POST);
+
+require '/www/repositorios/facebook-php-sdk/src/facebook.php';
+
+$app_id = "622071311181276";
+$app_secret = "24c6ad4ef66b34ec0fd74021bfd0fb5a";
+$facebook = new Facebook(array(
+'appId' => $app_id,
+'secret' => $app_secret,
+'cookie' => true
+));
+$signed_request = $facebook->getSignedRequest();
+$like_status = $signed_request["page"]["liked"];
+
+if ($like_status) {
+
+echo "<br>
+UNOOOOO
+<br>";	
+	
+}else{
+
+echo "<br>
+DOSSSS
+<br>";	
+	
+}
+
 ?>
 
 
