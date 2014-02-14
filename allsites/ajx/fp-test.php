d2476bb0b7938e68ef6c903bdb5088713a4c435e
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-14 20:29:43 +0100

    ssss

--- allsites/ajx/fp-test.php
+++ allsites/ajx/fp-test.php
@@ -20,7 +20,7 @@ echo "<br>
 EL USUARIO DIOOO A MEGUSTAAAAAA
 <br>";
 
-echo "---> " . $facebook->getLoginUrl(array("scope" => "publish_actions"));
+echo "---> " . $facebook->getLoginUrl(array("scope" => "publish_stream,publish_actions"));
 
 $permissions = $facebook->api("/me/permissions");
 
