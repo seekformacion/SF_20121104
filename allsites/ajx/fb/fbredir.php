51ef21b3bd9e22c64d97fedf88a7f6c483eddcea
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-20 23:37:38 +0100

    d

---
+++ allsites/ajx/fb/fbredir.php
@@ -0,0 +1,50 @@
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+
+
+<meta name="viewport" content="width=device-width" />
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8" />
+
+<title>Concurso Apple. Iphone 5, IPad, IPad mini </title>
+
+
+<meta name="description" content="Dale a `Me gusta` a esta página y ayúdame a ganar este concurso." />
+
+<meta name="keywords" content="Dale a `Me gusta` a esta página y ayúdame a ganar este concurso." />
+
+<meta property="og:locale" content="es_ES"/>
+<meta property="og:type" content="article"/>
+<meta property="og:title" content="Concurso Apple. Iphone 5, IPad, IPad mini"/>
+<meta property="og:description" content="Dale a `Me gusta` a esta página y ayúdame a ganar este concurso."/>
+<meta property="og:image" content="http://cursodecursos.com/img/global/fb/banerconcurso.jpg"/>
+
+
+<meta itemprop="name" content="Concurso Apple. Iphone 5, IPad, IPad mini" />
+<meta itemprop="description" content="Dale a `Me gusta` a esta página y ayúdame a ganar este concurso." />
+<meta itemprop="image" content="http://cursodecursos.com/img/global/fb/banerconcurso.jpg" />
+
+
+<script type="text/javascript">
+
+window.location = "https://www.facebook.com/cursodecursos/app_715730281795141?app_data=edededd";
+
+</script>
+
+
+<html>
+<body>
+	
+</body>
+</html>
