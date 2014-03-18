98399d1eef2461e9daf3b2fa58d4eb04406a1a69
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-18 17:34:20 +0100

    sss

--- allsites/objt/php/cursos/allids/claves.php
+++ allsites/objt/php/cursos/allids/claves.php
@@ -8,7 +8,7 @@ $Datos['cur_dirigidoa']=$datCur['cur_dirigidoa'];
 $codO=array('[|br]','[br]','[|str]','[str]','[|p]','[p]','[|em]','[em]');
 $codN=array('</br>','<br>','</strong>','<strong>','</p>','<p>','</em>','<em>');
 
-$Datos['temario']=str_replace($codO, $codN, strip_tags($datCur['temario'])) . '</em></p></strong>';
+//$Datos['temario']=str_replace($codO, $codN, strip_tags($datCur['temario'])) . '</em></p></strong>';
 
-
+$Datos['temario']=$datCur['temario'];
 ?>
