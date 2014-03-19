086766db665ab88ee051aedc6a0f59086357268b
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-19 12:59:15 +0100

    ddd

---
+++ allsites/sitemaps/cupones.php
@@ -0,0 +1,68 @@
+<?php
+set_time_limit(0);
+ini_set("memory_limit", "-1");
+
+
+include('/www/dbA.php');
+
+$v['debug']=0;
+$v['admin']=0;
+
+$v['conf']['state']=1; # 1=test 2=produccion
+$v['conf']['mode']=1; # 1=local 2=cloud
+$v['where']['idp']=						1; #### ID DEL PORTAL PARA TABLA urls
+$v['where']['view']=					'sitemaps'; #### ID DEL PORTAL PARA TABLA urls
+$v['where']['id']=					    '1'; 
+$v['where']['site']=					"cursodecursos.com";
+
+
+
+$v['path']['bin']=$v['path']['repo'] .	"/SF_20121104";
+$v['path']['fw']=$v['path']['repo'] .	"/FrameW_1";
+$v['path']['img']=$v['path']['repo'] .	"/SeekFormacion_images";
+
+$v['path']['baseURLskin'][1]=""; ## baseURL del SKIN local
+$v['path']['baseURLskin'][2]="http://s3-eu-west-1.amazonaws.com/seekf"; ## baseURL del SKIN en CLOUD
+
+
+
+
+
+
+
+include('funcSitemaps.php');  
+require_once $v['path']['fw'] . '/core/templates/paths.php';
+
+includeINIT('vars');
+includeINIT('provins');
+includeINIT('config');
+includeCORE('db/dbfuncs');
+includeCORE('templates/templates');
+includeCORE('funcs/general');
+
+
+
+
+$dcats=DBselect("select * from skf_datCupon_cur where done=0 GROUP BY cent, id_dat_cupon, cur;");
+
+
+print_r($dcats);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
