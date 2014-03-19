d88d10af51cedbd0a95b2fc2b7ca3070bb57ba17
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-19 13:37:14 +0100

    ss

--- allsites/sitemaps/cupones.php
+++ allsites/sitemaps/cupones.php
@@ -45,8 +45,27 @@ includeCORE('funcs/general');
 
 $dcats=DBselect("select * from skf_datCupon_cur where done=0 GROUP BY cent, id_dat_cupon, cur;");
 
+foreach ($dcats as $key => $vals) {
+$idc=$vals['cent']; $idcur=$vals['cur']; $idcup=$vals['id_dat_cupon']; $idins=$vals['id'];	
 
-print_r($dcats);
+$datos[$idc][$idcup][$idcur]=$idins;
+	
+}
+
+$date=date('Y') . date('m') . date('d');
+
+print_r($datos);
+
+if(count($datos)>0){
+foreach ($datos as $idcent => $cups){foreach ($cups as $idcupon => $curs){foreach($curs as $idcurso => $idins){
+$err="";
+$err=DBUpInsSDB("INSERT INTO skP_cupones (id_cent,id_cupon,fecha,id_curso) VALUES ($idcent,$idcupon,$date,$idcurso);",'seekpanel');		
+if(!$err){DBUpIns("UPDATE skf_datCupon_cur SET done=1 WHERE id=$idins;");};
+
+sendCupon($idcent,$idcupon,$idcurso);
+
+		
+}}}}
 
 
 
@@ -55,8 +74,11 @@ print_r($dcats);
 
 
 
-
-
+function sendCupon($idcent,$idcupon,$idcurso){
+	
+	
+	
+}
 
 
 
