013b19b4159d0962e7addaca31e85ff738097a29
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-23 16:16:27 +0100

    dd

---
+++ allsites/sitemaps/cambios.php
@@ -0,0 +1,45 @@
+<?php
+set_time_limit(0);
+ini_set("memory_limit", "-1");
+
+
+global $v;
+
+
+include('/www/dbA.php');
+$v['debug']=0;
+$v['admin']=0;
+$v['conf']['state']=1; # 1=test 2=produccion
+$v['conf']['mode']=1; # 1=local 2=cloud
+$v['where']['idp']=						1; #### ID DEL PORTAL PARA TABLA urls
+$v['where']['view']=					'sitemaps'; #### ID DEL PORTAL PARA TABLA urls
+$v['where']['id']=					    '1'; 
+$v['where']['site']=					"cursodecursos.com";
+$v['path']['bin']=$v['path']['repo'] .	"/SF_20121104";
+$v['path']['fw']=$v['path']['repo'] .	"/FrameW_1";
+$v['path']['img']=$v['path']['repo'] .	"/SeekFormacion_images";
+$v['path']['baseURLskin'][1]=""; ## baseURL del SKIN local
+$v['path']['baseURLskin'][2]="http://s3-eu-west-1.amazonaws.com/seekf"; ## baseURL del SKIN en CLOUD
+
+include('funcCambios.php');  
+require_once $v['path']['fw'] . '/core/templates/paths.php';
+
+includeINIT('vars');
+includeINIT('provins');
+includeINIT('config');
+includeCORE('db/dbfuncs');
+includeCORE('templates/templates');
+includeCORE('funcs/general');
+
+echo "\n\n";
+
+
+######## cursos nuevos
+$nue=DBselectSDB("SELECT id, act_id from skP_actions WHERE accion=1 ORDER BY datestamp",'seekpanel');
+
+print_r($nue);
+##############3
+
+
+
+?>
