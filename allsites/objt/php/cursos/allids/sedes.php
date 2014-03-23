91a472dcc2fef4c3cba01c0f173bae878add2e3c
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-23 19:21:46 +0100

    ddd

--- allsites/objt/php/cursos/allids/sedes.php
+++ allsites/objt/php/cursos/allids/sedes.php
@@ -14,7 +14,8 @@ $eqp=$v['vars']['provN'];
 
 $idcentroS=$datCur['id_centro'];
 $cps=DBselect("SELECT idpro FROM skv_relCurPro WHERE idcur=$idcur;");
-$cps=DBselect("SELECT distinct provincia FROM skv_sedes WHERE idcentro=$idcentroS ORDER BY nombre;");$cc=0;
+//$cps=DBselect("SELECT distinct provincia FROM skv_sedes WHERE idcentro=$idcentroS ORDER BY nombre;");
+$cc=0;
 
 
 if(count($cps)>0){
