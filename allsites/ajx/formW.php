c1f29d56be36d6acbef7bfe63a9c3d6e44120d29
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-05-08 17:29:53 +0200

    eee

--- allsites/ajx/formW.php
+++ allsites/ajx/formW.php
@@ -1,6 +1,22 @@
 <?php
 
-require_once ('form.php');
+
+foreach($_GET as $nombre_campo => $valor){  $asignacion = "\$" . $nombre_campo . "='" . $valor . "';";   eval($asignacion);};
+
+$v['where']['view']='categorias';
+$v['where']['id']=1; 
+require_once ('iniAJX.php');
+
+
+
+$file = fopen ("form.php?uid=$uid&idc=$idc", "r");
+while (!feof ($file)) { $form = fgets ($file, 1024);};
+fclose($file);
+
+$form=json_decode($form, true);
+$html=$form['html'];
+
+echo $html;
 
 
 ?>
