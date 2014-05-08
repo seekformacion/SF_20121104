6dfabcc21861d5fde93b545e408137d39d0792fc
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-05-08 17:32:29 +0200

    eee

--- allsites/ajx/formW.php
+++ allsites/ajx/formW.php
@@ -13,8 +13,10 @@ $file = fopen ("http://cursodecursos.com:8080/ajx/form.php?uid=$uid&idc=$idc", "
 while (!feof ($file)) { $form = fgets ($file, 1024);};
 fclose($file);
 
-$form=json_decode($form, true);
-$html=$form['html'];
+$form2=json_decode($form, true);
+print_r($form2);
+
+$html=$form2['html'];
 
 echo $html;
 
