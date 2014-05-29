1367857d990b125c821af7876621db50bb446776
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-05-29 13:37:32 +0200

    ddd

--- allsites/sitemaps/logos.php
+++ allsites/sitemaps/logos.php
@@ -6,15 +6,22 @@ ini_set("memory_limit", "-1");
 global $v;
 include('/www/dbA.php');
 
+$excludes['seekformacion.com']=1;
+$excludes['publiactive.es']=1;
+$excludes['publiactive.net']=1;
+$excludes['skin.php']=1;
+$excludes['sitemap.xml']=1;
+
 
 ######### portales
 $myDirectory=str_replace("/" . $v['where']['site'],'',$v['path']['httpd']);
 echo "\n";
 foreach(glob("$myDirectory/*") as $file) {
-//$file=str_replace($myDirectory, '', $file);
-echo $file . "\n";
+$file=str_replace($myDirectory . "/", '', $file);
+if(!array_key_exists($file, $excludes)){$sites[]=$file;}
 }
 
+print_r($sites);
 
 
 
