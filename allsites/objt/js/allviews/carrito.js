22af44d7098a30bebb4cacb1d7e58a0de710b996
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-06-06 16:27:44 +0200

    ddd

--- allsites/objt/js/allviews/carrito.js
+++ allsites/objt/js/allviews/carrito.js
@@ -449,7 +449,7 @@ for (i=1 ; i <= 50; i++){
 	}else if(i==10){if(!val){var no6='Campos obligatorios';}
 	
 	}else if(i==8){if (!val) {var no8='Campos obligatorios';}
-	}else if(i==26){if (!val){var no8='Campos obligatorios';}
+	}else if((i==26)&&(document.getElementById('df_26'))){if (!val){var no8='Campos obligatorios';}
 	
 	}else if(i==11){if(!val){var no11='Campos obligatorios';}	
 		
