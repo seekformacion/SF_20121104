a0d9e17c4603a29b3725233e48419fa34ee2fb02
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-06-10 16:17:03 +0200

    sss

--- allsites/sitemaps/funcSitemaps.php
+++ allsites/sitemaps/funcSitemaps.php
@@ -12,7 +12,8 @@ function GetURLtoCACHE($idp){global $v;
 #################3 compruebo numero de url a realizar por iteracion.. 720 interaciones en 5 dias cada 10 min
 $limit=10;
 $dcats=DBselect("select count(id) as tot from util_sitemap where idp IN ($idp);");
-if(array_key_exists(1, $dcats)){$tot=$dcats[1]['tot'];$limit=round(($tot/720),0);} #$limit=100;
+if(array_key_exists(1, $dcats)){$tot=$dcats[1]['tot'];$limit=round(($tot/720),0);};
+$limit=100;
 
 $dt=date('Y') . date('m') . date('d');
 $dcats=DBselect("select id, url, t_id, tipo, idp from util_sitemap where idp IN ($idp) AND done = 0 ORDER BY id DESC limit $limit;");
